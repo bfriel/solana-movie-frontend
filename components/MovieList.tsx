ff4440a68f339d1dc9bf1aa9cb368695532c93a9
Brian Friel <briandfriel@gmail.com>
2022-07-17 16:27:53 -0700

    feat: complete deserialization section

--- components/MovieList.tsx
+++ components/MovieList.tsx
@@ -1,25 +1,32 @@
-import { Card } from './Card'
-import { FC, useEffect, useState } from 'react'
-import { Movie } from '../models/Movie'
+import { Card } from "./Card";
+import { FC, useEffect, useState } from "react";
+import { Movie } from "../models/Movie";
+import { clusterApiUrl, Connection, PublicKey } from "@solana/web3.js";
 
-const MOVIE_REVIEW_PROGRAM_ID = 'CenYq6bDRB7p73EjsPEpiYN7uveyPUTdXkDkgUduboaN'
+const MOVIE_REVIEW_PROGRAM_ID = new PublicKey(
+  "CenYq6bDRB7p73EjsPEpiYN7uveyPUTdXkDkgUduboaN"
+);
 
 export const MovieList: FC = () => {
-    const [movies, setMovies] = useState<Movie[]>([])
+  const [movies, setMovies] = useState<(Movie | null)[]>([]);
 
-    useEffect(() => {
-        setMovies(Movie.mocks)
-    }, [])
-    
-    return (
-        <div>
-            {
-                movies.map((movie, i) => {
-                    return (
-                        <Card key={i} movie={movie} />
-                    )
-                })
-            }
-        </div>
-    )
-}
+  useEffect(() => {
+    const connection = new Connection(clusterApiUrl("devnet"));
+    connection
+      .getProgramAccounts(MOVIE_REVIEW_PROGRAM_ID)
+      .then(async (accounts) => {
+        const movies: (Movie | null)[] = accounts.map(({ account }) =>
+          Movie.deserialize(account.data)
+        );
+        setMovies(movies);
+      });
+  }, []);
+
+  return (
+    <div>
+      {movies.map((movie, i) => {
+        return <Card key={i} movie={movie} />;
+      })}
+    </div>
+  );
+};
